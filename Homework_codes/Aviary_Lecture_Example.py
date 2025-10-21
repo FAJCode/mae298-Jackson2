dd2c998b6c815c7021453a189bbf39fe2e0e9777
FAJCode <cjworkbusiness@gmail.com>
2025-10-21 13:34:13 -0700

    adding another folder

---
+++ Homework_codes/Aviary_Lecture_Example.py
@@ -0,0 +1,7 @@
+#In class example of aviary and class function
+import aviary.api as av
+import openmdao.api as om
+from aviary.variable_info.variables import Mission
+from cost_externalsubsystem.example_simplecostaviary_varibales import Aircraft
+
+
---
+++ Homework_codes/HelloWorld_OPENMDAO.py
@@ -0,0 +1,29 @@
+#Working example of OPENMDAO
+import openmdao.api as om
+
+# build the model
+prob = om.Problem()
+
+prob.model.add_subsystem('paraboloid', om.ExecComp('f = (x-4)**2 + x*y + (y+3)**2 - 3'))
+
+# setup the optimization
+prob.driver = om.ScipyOptimizeDriver()
+prob.driver.options['optimizer'] = 'Nelder-Mead'
+
+prob.model.add_design_var('paraboloid.x', lower=-50, upper=50)
+prob.model.add_design_var('paraboloid.y', lower=-50, upper=50)
+prob.model.add_objective('paraboloid.f')
+
+prob.setup()
+
+# Set initial values.
+prob.set_val('paraboloid.x', 3.0)
+prob.set_val('paraboloid.y', -4.0)
+
+# run the optimization
+prob.run_driver()
+
+#Get the results
+print('Optimal x value: ', prob.get_val('paraboloid.x'))
+print('Optimal y value: ', prob.get_val('paraboloid.y'))
+print('Objective value: ', prob.get_val('paraboloid.f'))
